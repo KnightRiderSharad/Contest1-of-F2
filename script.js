94e0623849026a036bf3ebb5e5d2f54d992e7380
KnightRiderSharad <ksharad34@gmail.com>
2023-01-07 17:14:11 +0530

    completed

--- script.js
+++ script.js
@@ -6,22 +6,95 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+// let nameInput = document.getElementById("nameInput");
+// let professionInput = document.getElementById("professionInput");
+// let ageInput = document.getElementById("ageInput");
+
+
+//1. console.log all the employees who have the profession of a developer, 
+// Using the map function.
 function PrintDeveloperswithMap() {
-  //Write your code here
+  arr.map((items)=>{
+    if( items.profession=== "developer" ){
+      console.log(items);
+  }
+  })
 }
+//PrintDeveloperswithMap()         
 
+
+
+//2. console.log all the employees who have the profession of a developer,
+// Using the for each function. 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach((items2)=>{
+    if( items2.profession=== "developer" ){
+      console.log(items2);
+  }
+  })
 }
+//PrintDeveloperbyForEach()          
 
-function addData() {
-  //Write your code here
+
+//3. In this function, make another such employee object and append that to this array given below and then console.log it.
+let arrayAdd= [{ 
+  id:4,
+  name:"susan",
+  age:"20",
+  profession:"intern"
+} ]
+
+function addData(){
+//   arrayAdd.forEach((item)=>{
+//   let Employee= document.createElement("div");
+//   Employee.innerHTML= `${item.id} &nbsp;&nbsp; Name: ${item.name} &nbsp; Age: ${item.age}  &nbsp; Profession: ${item.profession}`;
+//   arrayAdd.append(Employee);
+//   console.log(addData)
+//   })
+// addData()
+
+
+let Employee= { 
+                id: 5, 
+                name: "cena", 
+                age: "15", 
+                profession:"intern" 
+              }; 
+      arrayAdd.push(Employee);
+      console.log(arrayAdd);
 }
+//addData()                           
 
+
+//4. In this function, remove the object where the profession is admin. console.log the changed array.
 function removeAdmin() {
-  //Write your code here
+   // Using the filter function
+   let changedArr = arr.filter((items)=>{    
+   if (items.profession !== "admin"){
+    return items;
+   }
+   })  
+   console.log(changedArr)
 }
+//removeAdmin()                         
+
+
+
+//5. concatenate 2 arrays and console.log.
+let arr2 = [
+  { id: 6, name: "Amar", age: "20", profession: "developer" },
+  { id: 7, name: "Akbar", age: "21", profession: "intern" },
+  { id: 8, name: "Anthony", age: "22", profession: "admin" },
+];
 
 function concatenateArray() {
-  //Write your code here
-}
+
+//   1. one way of Method -
+        arrNew1 = arr.concat(arr2);       //Concatenating Two Arrays
+        console.log(arrNew1);
+
+//   2. second way of Method -
+    //  arrayC = [...arr, ...arr2];       //Spread Operator
+    //  console.log(arrayC)
+ }
+ //concatenateArray()                    
